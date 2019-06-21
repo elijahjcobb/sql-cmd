5ee4d74bde6973164e6d659a50eb85d2bfcbd696
Elijah Cobb <elijah@elijahcobb.com>
2019-06-21 11:51:59 -0400

    Early beta version.

--- ts/index.ts
+++ ts/index.ts
@@ -22,4 +22,220 @@
  *
  */
 
-console.log("Hello, world!");
+import { ECMap, ECArrayList } from "@elijahjcobb/collections";
+
+enum ECSQLCMDMethod {
+	select = "SELECT",
+	update = "UPDATE",
+	insert = "INSERT",
+	delete = "DELETE"
+}
+
+type ECSQLCMDSort = "<" | ">";
+type ECSQLCMDOperator = "=" | "!=" | ">" | "<" | ">=" | "<=" | "in" | "like";
+type ECSQLCMDValue = string | number | boolean | undefined | null;
+
+const escapeValue: (value: ECSQLCMDValue) => string | number | boolean = (value: ECSQLCMDValue): string | number | boolean => {
+
+	if (value === null || value === undefined) return "NULL";
+	else if (typeof value === "string") {
+
+		let escaped: string = value;
+
+		escaped = escaped.replace(RegExp(`'`, "g"), `\\'`);
+		escaped = escaped.replace(RegExp(`"`, "g"), `\\"`);
+		escaped = `'${escaped}'`;
+
+		return escaped;
+
+	} else return value;
+
+};
+
+export class ECSQLCMD {
+
+	private readonly method: ECSQLCMDMethod;
+	private table: string | undefined;
+	private orderings: { key: string, direction: ECSQLCMDSort}[] = [];
+	private limitOfItems: number | undefined;
+	private parameters: ECMap<string, ECSQLCMDValue> = new ECMap<string, ECSQLCMDValue>();
+	private queries: ECSQLCMDQuery | undefined;
+
+	private constructor(method: ECSQLCMDMethod) { this.method = method; }
+
+	public from(table: string): ECSQLCMD {
+
+		this.table = table;
+		return this;
+
+	}
+
+	public into(table: string): ECSQLCMD {
+
+		this.table = table;
+		return this;
+
+	}
+
+	public in(table: string): ECSQLCMD {
+
+		this.table = table;
+		return this;
+
+	}
+
+	public sort(key: string, direction: ECSQLCMDSort): ECSQLCMD {
+
+		this.orderings.push({ key, direction });
+		return this;
+
+	}
+
+	public limit(limit: number): ECSQLCMD {
+
+		this.limitOfItems = limit;
+		return this;
+
+	}
+
+	public set(key: string, value: ECSQLCMDValue): ECSQLCMD {
+
+		this.parameters.set(key, value);
+		return this;
+
+	}
+
+	public generate(): string {
+
+		let command: string = "";
+
+		if (this.method === ECSQLCMDMethod.select) {
+
+			command = `SELECT * FROM ${this.table}`;
+			if (this.queries) command += ` WHERE ${this.queries.generate()}`;
+			if (this.orderings.length > 0) {
+
+				command += ` ORDER BY `;
+				const formattedOrders: string[] = [];
+
+				this.orderings.forEach((ordering: { key: string, direction: ECSQLCMDSort}) => {
+
+					formattedOrders.push(`${ordering.key} ${ordering.direction === "<" ? "ASC" : "DESC"}`);
+
+				});
+
+				command += formattedOrders.join(", ");
+				if (this.limitOfItems !== undefined) command += ` LIMIT ${this.limitOfItems}`;
+
+			}
+
+		} else if (this.method === ECSQLCMDMethod.insert) {
+
+			command = `INSERT INTO ${this.table}`;
+			if (this.queries) command += ` WHERE ${this.queries.generate()}`;
+			if (this.parameters.size() === 0) throw new Error("You must specify at least one parameter to insert.");
+
+			command += ` (${this.parameters.keys().toString(", ")}) VALUES (`;
+			command += this.parameters.values().map((value: ECSQLCMDValue) => { return escapeValue(value); }).toString(", ");
+			command += ")";
+
+		} else if (this.method === ECSQLCMDMethod.delete) {
+
+			command = `DELETE FROM ${this.table}`;
+			if (this.queries) command += ` WHERE ${this.queries.generate()}`;
+
+		} else if (this.method === ECSQLCMDMethod.update) {
+
+			command += `UPDATE ${this.table} SET`;
+
+			const updateItems: string[] = [];
+
+			this.parameters.forEach((key: string, value: ECSQLCMDValue) => {
+
+				updateItems.push(`${key}=${escapeValue(value)}`);
+
+			});
+
+			command += ` ${updateItems.join(", ")}`;
+
+			if (this.queries) command += ` WHERE ${this.queries.generate()}`;
+
+		}
+
+		command += ";";
+
+		return command;
+
+	}
+
+	public where(query: ECSQLCMDQuery): ECSQLCMD {
+
+		this.queries = query;
+		return this;
+
+	}
+
+	public static select(): ECSQLCMD { return new ECSQLCMD(ECSQLCMDMethod.select); }
+	public static update(): ECSQLCMD { return new ECSQLCMD(ECSQLCMDMethod.update); }
+	public static insert(): ECSQLCMD { return new ECSQLCMD(ECSQLCMDMethod.insert); }
+	public static delete(): ECSQLCMD { return new ECSQLCMD(ECSQLCMDMethod.delete); }
+
+}
+
+type ECSQLCMDQueryCondition = "AND" | "OR";
+type ECSQLCMDQueryItems = {
+	key: string,
+	operator: ECSQLCMDOperator,
+	value: ECSQLCMDValue
+};
+
+export class ECSQLCMDQuery {
+
+	private condition: ECSQLCMDQueryCondition | undefined;
+	private items: ECArrayList<ECSQLCMDQueryItems | ECSQLCMDQuery> = new ECArrayList<ECSQLCMDQueryItems | ECSQLCMDQuery>();
+
+	public where(key: string, operator: ECSQLCMDOperator, value: ECSQLCMDValue): ECSQLCMDQuery {
+
+		this.items.add({key, operator, value});
+		return this;
+
+	}
+
+	public whereThese(query: ECSQLCMDQuery): ECSQLCMDQuery {
+
+		this.items.add(query);
+		return this;
+
+	}
+
+	public generate(): string {
+
+		const parts: string[] = [];
+
+		this.items.forEach((item: ECSQLCMDQueryItems | ECSQLCMDQuery) => {
+
+			if (item instanceof ECSQLCMDQuery) parts.push(item.generate());
+			else parts.push(item.key + item.operator + escapeValue(item.value));
+
+		});
+
+		const command: string = parts.join(` ${this.condition} `);
+		return `(${command})`;
+
+	}
+
+
+	private static builder(condition: ECSQLCMDQueryCondition): ECSQLCMDQuery {
+
+		const q: ECSQLCMDQuery = new ECSQLCMDQuery();
+		q.condition = condition;
+
+		return q;
+
+	}
+
+	public static and(): ECSQLCMDQuery { return this.builder("AND"); }
+	public static or(): ECSQLCMDQuery { return this.builder("OR"); }
+
+
+}
